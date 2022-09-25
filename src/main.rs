1899f6338a925ea929ce823f80f6a46bff656c58
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-09-25 12:51:48 -0700

    add minimal rust kernel and make compilation easy

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World";
+
 // Disable name mangling to ensure that Rust really
 // outputs a function with the name _start, without
 // the attribute the compiler would generate some 
@@ -18,5 +20,24 @@ fn panic(_info: &PanicInfo) -> ! {
 // it should use the C calling convention for this
 // function
 pub extern "C" fn _start() -> ! {
-    loop {}
+    // This function is the entry point, since the
+    // linker looks for a function named `_start` 
+    // by default.
+
+    // VGA buffer starts at 0xb8000
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    // Foreach byte in HELLO
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            // Write the string byte
+            *vga_buffer.offset(i as isize * 2) = byte;
+            // Write the color byte (0xb is light cyan)
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {
+        
+    }
 }
