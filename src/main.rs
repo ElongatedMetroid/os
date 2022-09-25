dada346e821e729fa81fab7aa2933118503cf9cd
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-09-25 11:39:28 -0700

    create freestanding rust binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// Disable name mangling to ensure that Rust really
+// outputs a function with the name _start, without
+// the attribute the compiler would generate some 
+// random name.
+#[no_mangle]
+// Mark as `extern "C"` to tell the compiler that 
+// it should use the C calling convention for this
+// function
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
